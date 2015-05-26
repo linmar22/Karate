e78c109a37149fe37e1a2b6ac1a72a69b7ee79c3
linas <linmar22>
2015-05-26 08:02:06 +0200

    Fancy entry animation

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -3,17 +3,29 @@
  */
 $(document).ready(function () {
 
+    var screenHeight = $(window).height();
+
+    function changeWhite() {
+        $("#header").animate({backgroundColor: '#FFFFFF'},1000);
+        $("#footer").animate({backgroundColor: '#FFFFFF'},1000, function(){
+            window.location.href = "http://www.google.com";
+        });
+    }
+
+
     $(".link").click(function () {
-        window.location.href = "http://www.google.com";
+        $("#header").animate({height: screenHeight / 2},1000);
+        $("#footer").animate({height: screenHeight / 2},1000, function(){
+            changeWhite();
+        });
     });
 
     $("#content_center_wrapper").hover(
-        function(){
+        function () {
             $("#enter").css({opacity: 0});
             $("#enter").removeClass("intro");
-        },function(){
+        }, function () {
             $("#enter").css({opacity: 1});
         }
     );
-
 });
