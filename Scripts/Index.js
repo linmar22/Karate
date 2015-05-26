f1d1b50923e4ee6a36ec208378bc827fefbc89b0
linas <linmar22>
2015-05-26 04:58:45 +0200

    Implemented implementation of responsive responsivenes. Changed font. Added script and jquery.

---
+++ Scripts/Index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by linas on 5/26/15.
+ */
+$(document).ready(function(){
+
+    $(".link").click(function(){
+        window.location.href = "http://www.google.com";
+    });
+
+});
