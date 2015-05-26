cf27ecd04ffb8f494a016567eb8ee02f83642872
linas <linmar22>
2015-05-26 07:23:35 +0200

    Enter 'thingy'.

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,10 +1,19 @@
 /**
  * Created by linas on 5/26/15.
  */
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".link").click(function(){
+    $(".link").click(function () {
         window.location.href = "http://www.google.com";
     });
+
+    $("#content_center_wrapper").hover(
+        function(){
+            $("#enter").css({opacity: 0});
+            $("#enter").removeClass("intro");
+        },function(){
+            $("#enter").css({opacity: 1});
+        }
+    );
 
 });
